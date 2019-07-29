1cc146201a5a8a54a0185487154939c3504e7fbe
Brandon <brandontkessler@gmail.com>
2019-07-29 11:20:41 -0700

    Added error routes/templates

--- app/main/views.py
+++ app/main/views.py
@@ -13,3 +13,19 @@ def index():
 @check_confirmed
 def secret():
     return "SECRET PAGE"
+
+
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html', e=e), 404
+
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
+
+
+@main.app_errorhandler(403)
+def page_not_found(e):
+    return render_template('errors/403.html'), 403
